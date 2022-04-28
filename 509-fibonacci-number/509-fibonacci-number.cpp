06cd335ca75367bcbb089bf80a687e60955e56e3
Saurav Sutaria <73012044+Saurav-Sutaria@users.noreply.github.com>
2022-04-28 22:50:03 +0530

    Time: 3 ms (55.31%), Space: 5.9 MB (42.30%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 || n == 1) return n;
+        int first = 0, second = 1, third = 0;
+        for(int i=2;i<=n;i++){
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        return third;
+    }
+};
