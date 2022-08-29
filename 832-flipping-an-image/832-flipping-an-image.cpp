9cd344d92fdfcefef041993662baa1cd854dd0b1
Saurav Sutaria <73012044+Saurav-Sutaria@users.noreply.github.com>
2022-08-29 18:52:18 +0530

    Time: 15 ms (9.72%), Space: 8.7 MB (92.92%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.cpp
+++ 832-flipping-an-image/832-flipping-an-image.cpp
@@ -4,16 +4,15 @@ public:
         int n = image.size();
         for(int i=0;i<n;i++){
             int j = 0, k = n-1;
-            while( j  < k){
+            while( j < k){
                 swap(image[i][j],image[i][k]);
+                image[i][j] ^= 1;
+                image[i][k] ^= 1;
                 j++;k--;
             }
+            if(j == k) image[i][j] ^= 1;
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                image[i][j] ^= 1;
-            }
-        }
+        
         return image;
     }
 };
